f556a1586db37aeae03382974152e97405ee8cde
therohitkumargang <shyam000vns@gmail.com>
2025-02-16 13:18:29 +0530

    add new code

--- src/java8program/TestingDemo.java
+++ src/java8program/TestingDemo.java
@@ -3,11 +3,10 @@ package java8program;
 public class TestingDemo {
 
 	public static void main(String[] args) {
-	
-		
+
 		System.out.println("Hii my name is shyamnarayan singh");
 		System.out.println("Hello how are you");
-		System.out.println("Byy take care");
+
 		System.out.println("Welcome");
 
 	}
