68aa3a44beaa933b01eb1d814d63f79af2ba795a
therohitkumargang <shyam000vns@gmail.com>
2025-02-16 11:54:02 +0530

    Initial commit

---
+++ src/java8program/TestingDemo.java
@@ -0,0 +1,14 @@
+package java8program;
+
+public class TestingDemo {
+
+	public static void main(String[] args) {
+	
+		
+		System.out.println("Hii my name is shyamnarayan singh");
+		System.out.println("Hello how are you");
+		System.out.println("Byy take care");
+
+	}
+
+}
